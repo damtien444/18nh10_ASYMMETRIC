d4ad4db71e03252ed95ee9eab7731dc02d39e073
terrordrone4 <70142260+terrordrone4@users.noreply.github.com>
2020-11-20 23:35:28 +0700

    Detail-displaying activity only works with this "Product" class since field/setters name for "Color", "Gia", "Name",... must be identical to database's

---
+++ FashionBrandApp/app/src/main/java/com/lienhuong/fashionbrandapp/model/Product.java
@@ -0,0 +1,62 @@
+package com.lienhuong.fashionbrandapp.model;
+
+public class Product {
+    private String Color, Gia, Image, Mota, Name;
+    public Product(){
+
+    }
+    public Product(Product targetProductToCopyFrom) {
+        this.Color = targetProductToCopyFrom.Color;
+        this.Gia = targetProductToCopyFrom.Gia;
+        this.Image = targetProductToCopyFrom.Image;
+        this.Mota = targetProductToCopyFrom.Mota;
+        this.Name = targetProductToCopyFrom.Name;
+    }
+    public Product(String Color, String Gia, String Image, String Mota, String Name) {
+        this.Color = Color;
+        this.Gia = Gia;
+        this.Image = Image;
+        this.Mota = Mota;
+        this.Name = Name;
+    }
+
+    public String getcolor() {
+        return Color;
+    }
+
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+
+    public String getGia() {
+        return Gia;
+    }
+
+    public void setGia(String Gia) {
+        this.Gia = Gia;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String Image) {
+        this.Image = Image;
+    }
+
+    public String getMota() {
+        return Mota;
+    }
+
+    public void setMota(String Mota) {
+        this.Mota = Mota;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+}
