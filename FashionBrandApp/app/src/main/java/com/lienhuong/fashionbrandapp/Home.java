21d3f5b539017679b8f1b7e5ef954f965d310149
damtien444 <Damtien1>
2020-11-24 20:33:31 +0700

    Load data from firebase created Category

--- FashionBrandApp/app/src/main/java/com/lienhuong/fashionbrandapp/Home.java
+++ FashionBrandApp/app/src/main/java/com/lienhuong/fashionbrandapp/Home.java
@@ -4,12 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.lienhuong.fashionbrandapp.ViewHolder.MenuViewHolder;
 import com.lienhuong.fashionbrandapp.itemDetail.ItemDetailsDisplayerActivity;
+import com.lienhuong.fashionbrandapp.model.Category;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,17 +29,31 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Home extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+
+    FirebaseDatabase database;
+    DatabaseReference category;
+    TextView txtViewUsername;
+    RecyclerView recycler_menu;
+    
+    RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Shop us!");
         setSupportActionBar(toolbar);
+
+        database = FirebaseDatabase.getInstance();
+        category = database.getReference("Category");
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,7 +75,15 @@ public class Home extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+//        txtViewUsername = (TextView) findViewById(R.id.header_user_name);
+//        if (FirebaseAuth.getInstance().getCurrentUser().getDisplayName().isEmpty())
+//            txtViewUsername.setText("Ban than men");
+//        else txtViewUsername.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
